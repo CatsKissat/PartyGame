654f06aa3c153211f2f23ffb2367bd594a9a992f
Tomi Sepänmaa <tomi.sepanmaa@tuni.fi>
2023-06-16 15:41:12 +0300

    Added Metroidvania CharacterController. WIP: Use CharacterController on 3D space

---
+++ Assets/MetroidvaniaController/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool dash = false;
+
+    // New Input System
+    [SerializeField] private PlayerInput playerInput;
+    private InputAction inputAction;
+    private float moveInput;
+
+    //bool dashAxis = false;
+
+    private void OnValidate()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<float>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        horizontalMove = moveInput * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        //if ( Input.GetKeyDown(KeyCode.Z) )
+        //{
+        //    jump = true;
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+        //	dash = true;
+        //}
+
+        /*if (Input.GetAxisRaw("Dash") == 1 || Input.GetAxisRaw("Dash") == -1) //RT in Unity 2017 = -1, RT in Unity 2019 = 1
+		{
+			if (dashAxis == false)
+			{
+				dashAxis = true;
+				dash = true;
+			}
+		}
+		else
+		{
+			dashAxis = false;
+		}
+		*/
+
+    }
+
+    public void OnFall()
+    {
+        animator.SetBool("IsJumping", true);
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump, dash);
+        jump = false;
+        dash = false;
+    }
+}
