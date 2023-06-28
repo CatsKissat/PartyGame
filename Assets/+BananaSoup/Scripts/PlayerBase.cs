c76c535508bf405230b9b3dd4622d9b2b43575b4
Cats <tomi.sepanmaa@gmail.com>
2023-06-28 12:44:26 +0300

    Moved Player ID logic from PlayerSpriteSelector to PlayerBase

--- Assets/+BananaSoup/Scripts/PlayerBase.cs
+++ Assets/+BananaSoup/Scripts/PlayerBase.cs
@@ -1,6 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
 namespace BananaSoup.Units
 {
     public class PlayerBase : UnitBase
     {
+        private PlayerInput playerInput;
+        private int playerId;
+        private PlayerSpriteSelector playerSpriteSelector;
+
+        private void OnEnable()
+        {
+            GetInputReference();
+
+            Debug.Log($"PlayerID {playerId} joined the game.");
+        }
+
+        private void OnDisable()
+        {
+            Debug.Log($"PlayerID {playerId} left the game.");
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            GetReferences();
+            playerSpriteSelector.AssingSprite();
+        }
+
+        private void GetInputReference()
+        {
+            if ( playerInput == null )
+            {
+                playerInput = GetComponent<PlayerInput>();
+                playerId = playerInput.playerIndex;
+
+                if ( playerInput == null )
+                {
+                    Debug.LogError($"{name} is missing a PlayerInput!");
+                }
+            }
+        }
+
+        private void GetReferences()
+        {
+            playerSpriteSelector = GetComponent<PlayerSpriteSelector>();
+            if ( playerSpriteSelector == null )
+            {
+                Debug.Log($"{name} is missing a PlayerSpriteSelector!");
+            }
+        }
     }
 }
