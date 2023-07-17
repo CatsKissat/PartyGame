6afc44e199a13797a15844c82917606cbdf5ed22
Lauri <lauri.kalliola97@outlook.com>
2023-07-17 16:37:39 +0300

    Created a script which selects the placeable blocks for buildmode. More info ->
    - The script is attached to the PlaceableSelector.prefab
    - The script first takes a random int between 1 and 2 to select the amount of platform blocks that can show up on the selectables screen.
    - Then a random int between 1 and 3 to do the same for the amount of traps.
    - Then fills the rest with blocks.
    
    - There is no duplicate check for prefabs or trap modifiers at all.

---
+++ Assets/+BananaSoup/Scripts/PreBuildMode/SelectPlaceables.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using BananaSoup.Modifiers;
+using BananaSoup.Traps;
+using BananaSoup.Blocks;
+using BananaSoup.Platforms;
+using BananaSoup.Units;
+
+namespace BananaSoup.PreBuildMode
+{
+    public class SelectPlaceables : MonoBehaviour
+    {
+        [SerializeField, Tooltip("Trap prefabs")]
+        private TrapBase[] trapPrefabs;
+
+        [SerializeField, Tooltip("Block prefabs")]
+        private BlockBase[] blockPrefabs;
+
+        [SerializeField, Tooltip("Platform prefabs")]
+        private PlatformBase[] platformPrefabs;
+
+        [Space]
+
+        [SerializeField, Tooltip("Toggle debugging on or off.")]
+        private bool debugThis = false;
+
+        [SerializeField, ShowIf("debugThis")]
+        private int numberOfPlayers = 0;
+        [SerializeField, ShowIf("debugThis")]
+        private int extraPlaceables = 2;
+
+        private List<UnitBase> selectedPlaceables = new List<UnitBase>();
+
+        private int prefabsNeeded = 0;
+
+        private void Start()
+        {
+            prefabsNeeded = numberOfPlayers + extraPlaceables;
+
+            SelectPrefabs();
+        }
+
+        private void SelectPrefabs()
+        {
+            int platforms = Random.Range(1, 2);
+            prefabsNeeded -= platforms;
+
+            int traps = Random.Range(1, 3);
+            prefabsNeeded -= traps;
+
+            int blocks = prefabsNeeded;
+            prefabsNeeded = 0;
+            
+            GetRandomBlock(blocks);
+            GetRandomPlatform(platforms);
+            GetRandomTrap(traps);
+        }
+
+        private UnitBase GetRandomPrefab(UnitBase[] prefabs)
+        {
+            int randomIndex = Random.Range(0, prefabs.Length);
+            return prefabs[randomIndex];
+        }
+
+        private void GetRandomBlock(int amountToGet)
+        {
+            for ( int i = 0; i < amountToGet; i++ )
+            {
+                selectedPlaceables.Add(GetRandomPrefab(blockPrefabs)); 
+            }
+        }
+
+        private void GetRandomPlatform(int amountToGet)
+        {
+            for ( int i = 0; i < amountToGet; i++ )
+            {
+                selectedPlaceables.Add(GetRandomPrefab(platformPrefabs)); 
+            }
+        }
+
+        private void GetRandomTrap(int amountToGet)
+        {
+            for ( int i = 0; i < amountToGet; i++ )
+            {
+                selectedPlaceables.Add(GetRandomPrefab(trapPrefabs));
+            }
+        }
+    }
+}
