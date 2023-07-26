b0b4102c95f7971add4ec07ec52a221b5b1007a8
Lauri <lauri.kalliola97@outlook.com>
2023-07-26 18:34:37 +0300

    Reverted SetWorldRotation from Start to Update as it doesn't work as intended in Start.

--- Assets/+BananaSoup/Scripts/Utils/SetWorldRotation.cs
+++ Assets/+BananaSoup/Scripts/Utils/SetWorldRotation.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using NaughtyAttributes;
-using System.Collections;
 
 namespace BananaSoup.Utils
 {
@@ -8,23 +7,6 @@ namespace BananaSoup.Utils
     {
         [SerializeField, Tooltip("The desired worldrotation for the GameObject.")]
         private Vector3 rotation = Vector3.zero;
-        
-        // The time to delay start in deconds.
-        private float startDelay = 0.1f;
-
-        private Coroutine delayedStartRoutine = null;
-
-        /// <summary>
-        /// If delayed start isn't null when disabling the GameObject stop it and null it.
-        /// </summary>
-        private void OnDisable()
-        {
-            if ( delayedStartRoutine != null )
-            {
-                StopCoroutine(delayedStartRoutine);
-                delayedStartRoutine = null;
-            }
-        }
 
         /// <summary>
         /// Method assigned to a button to get the current rotation of the GameObject.
@@ -36,25 +18,11 @@ namespace BananaSoup.Utils
         }
 
         /// <summary>
-        /// The desired world rotation of the GameObject is set in start after a
-        /// desired delay (in seconds).
+        /// Keep updating the GameObjects world rotation to the desired rotation.
         /// </summary>
-        private void Start()
+        private void Update()
         {
-            if ( delayedStartRoutine == null )
-            {
-                delayedStartRoutine = StartCoroutine(DelayStart());
-            }
-
             transform.rotation = Quaternion.Euler(rotation);
-        }
-
-        /// <summary>
-        /// Coroutine used to delay Start().
-        /// </summary>
-        private IEnumerator DelayStart()
-        {
-            yield return new WaitForSeconds(startDelay);
         }
     }
 }
